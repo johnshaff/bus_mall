9b4832ef29ff5e8d2cd80a8fbe56dea413386e44
JOHN SHAFF <JOHNSHAFF@GMAIL.COM>
2016-05-23 14:53:49 -0700

    created all new instances

--- bus_mall.js
+++ bus_mall.js
@@ -5,6 +5,9 @@ var resultsSection = document.getElementById('resultsSection');
 var ps1 = document.getElementById('ps1');
 var ps2 = document.getElementById('ps2');
 var ps3 = document.getElementById('ps3');
+var rp1 = getElementById('rp1');
+var rp1 = getElementById('rp2');
+var rp1 = getElementById('rp3');
 
 
 var objectList = [];
@@ -18,10 +21,33 @@ function hotornot (imageName, filePath) {
   this.tallyClick = 0;
 };
 
-p1.addEventListener('click', clickHandler);
-p2.addEventListener('click', clickHandler);
-p3.addEventListener('click', clickHandler);
-showResults.addEventListener('click', resultsHandler);
+// p1.addEventListener('click', clickHandler);
+// p2.addEventListener('click', clickHandler);
+// p3.addEventListener('click', clickHandler);
+// showResults.addEventListener('click', resultsHandler);
+
+//creates new instances
+var bag = new hotornot ('bag', 'img/bag.png');
+var banana = new hotornot ('banana', 'img/banana.png');
+var bathroom = new hotornot ('bathroom', 'img/bathroom.png');
+var boots = new hotornot ('boots', 'img/boots.png');
+var breakfast = new hotornot ('breakfast', 'img/breakfast.png');
+var boots = new hotornot ('boots', 'img/boots.png');
+var bubblegum = new hotornot ('bubblegum', 'img/bubblegum.png');
+var chair = new hotornot ('chair', 'img/chair.png');
+var cthulhu = new hotornot ('cthulhu', 'img/cthulhu.png');
+var dog_duck  = new hotornot ('dog_duck', 'img/dog_duck.png');
+var dragon = new hotornot ('dragon', 'img/dragon.png');
+var pen = new hotornot ('pen', 'img/pen.png');
+var pet_sweep = new hotornot ('pet_sweep', 'img/pet_sweep.png');
+var scissors = new hotornot ('scissors', 'img/scissors.png');
+var shark = new hotornot ('shark', 'img/shark.png');
+var sweep = new hotornot ('sweep', 'img/sweep.png');
+var tauntaun = new hotornot ('tauntaun', 'img/tauntaun.png');
+var unicorn = new hotornot ('unicorn', 'img/unicorn.png');
+var usb = new hotornot ('usb', 'img/usb.png');
+var water_can = new hotornot ('water_can', 'img/water_can.png');
+var wine_glass = new hotornot ('wine_glass', 'img/wine_glass.png');
 
 //creates a random number
 var randomator = function () {
@@ -42,8 +68,9 @@ while (instance1 === instance2 === instance3) {
 
 //Populate choices with the pics from the 3 different objects
 var voterRefresh = function () {
-  var rp1 = getElementById('rp1');
   rp1.src = instance1.filePath;
+  rp2.src = instance2.filePath;
+  rp3.src = instance3.filePath;
 };
 
 // var zeroArrays = function() {
